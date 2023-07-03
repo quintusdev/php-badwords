7e2545c7e6f1043d9a508c8666722bc9c484c931
Quintusdev <quintusdev92@gmail.com>
2023-07-03 11:08:26 +0200

    Es. corretto - es. completato

--- myphp.php
+++ myphp.php
@@ -1,6 +1,5 @@
 <?php
     /* Nella prima parte dichiaro le variabili e recupero i dati inviati dall'INDEX */
-<<<<<<< HEAD
     $badword = $_POST['badword'];
     $censored = $_POST['censored'];
     /* inserisco in una variabile la lunghezza del testo */
@@ -9,13 +8,6 @@
     $censored_p = str_replace($censored, '***', $badword);
     /* lunghezza del paragrafo censurato */
     $length_p_cens = strlen($censored_p);
-=======
-    $text = $_POST['text'];
-
-    /* Prendo il valore inserito nella Textarea */
-    $censored = $_POST['censored'];
-
->>>>>>> 8a457ab1b52a1b187401415d324cafa260e6e1a2
 ?>
 
 <!DOCTYPE html>
@@ -34,7 +26,6 @@
         <div class="container w-100">
             <div class="row w-75 mx-auto">
                 <div class="col-12">
-<<<<<<< HEAD
                     <div>
                         <!-- In action richiamo il file PHP e uso il metodo POST per inviare i dati al back-end -->
                         <h5>Paragrafo:</h5>
@@ -53,17 +44,6 @@
                         <p><?php echo $censored_p; ?></p>
                         <!-- Visualizzo la lunghezza del paragrafo -->
                         <p><strong>Lunghezza del paragrafo: </strong> <?php echo $length_p_cens; ?></p>
-=======
-                    <div class="content">
-                        <h5>Paragrafo inserito:</h5>
-                        <span><?php echo $text ?></span>
-                        <br>
-                        <h5>Lunghezza caratteri inseriti:</h5>
-                        <span><?php echo strlen($text) ?></span>
-                        <br>
-                        <h5>Paragrafo censurato:</h5>
-                        <span><?php echo str_replace($censored, "***", $text) ?></span>
->>>>>>> 8a457ab1b52a1b187401415d324cafa260e6e1a2
                     </div>
                 </div>
             </div>
